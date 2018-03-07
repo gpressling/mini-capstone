85927cb072709248e031853f0740d460ec4df918
gpressling <greg.pressling@gmail.com>
2018-03-06 18:53:12 -0600

    created seeds file with products for my store

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Custom T-Shirt", price: 30, image_url: "https://www.google.com", description: "Create your own custom graphic T-Shirt")
+Product.create(name: "Custom Hat", price: 25, image_url: "https://www.google.com", description: "Create your own custom Hat")
+Product.create(name: "Custom Night stick", price: 1000, image_url: "https://www.google.com", description: "Don't get caught out after dark without your very own, CUSTOM NIGHT STICK")
