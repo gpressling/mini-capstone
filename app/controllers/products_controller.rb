061dcd2c7cdd256039c54fe73e812e8eb2d38b1b
gpressling <greg.pressling@gmail.com>
2018-03-06 19:15:40 -0600

    added routes file to return all products in json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+  def all_products
+    products = Product.all
+    product_hashes = []
+    products.each do |product|
+      product_hashes << {
+        name: product.name,
+        price: product.price,
+        image_url: product.image_url,
+        description: product.description
+      }
+    end
+    render json: product_hashes
+  end
 end
